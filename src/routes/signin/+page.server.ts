adc186d9a49dfe5325e8095374a9fde49cbfcc52
Leonardo Montini <lion.48m@gmail.com>
2023-04-20 21:10:00 +0200

    Connect signIn to AWS

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,9 +1,21 @@
+import { signIn } from '$lib/cognito';
+import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions = {
 	signIn: async ({ request }) => {
 		const data = await request.formData();
 		console.log('*****Data: ', data);
-		return { signin: false };
+		const email = data.get('email') as string;
+		const password = data.get('password') as string;
+
+		try {
+			console.log('Trying to sign in');
+			await signIn(email, password);
+			return { signin: true };
+		} catch (e: any) {
+			console.log('Error: ', e);
+			return fail(400, { error: e?.message ?? 'error' });
+		}
 	}
 } satisfies Actions;
--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -2,6 +2,8 @@
 	import Avatar from '../../components/avatar/Avatar.svelte';
 
 	let closeEyes = false;
+
+	export let form;
 </script>
 
 <svelte:head>
@@ -24,7 +26,12 @@
 					<h1 class="text-2xl font-semibold">Enter your user and password</h1>
 				</div>
 				<div class="divide-y divide-gray-200">
-					<div class="pt-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+					<pre>{JSON.stringify(form)}</pre>
+					<form
+						class="pt-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+						method="POST"
+						action="?/signIn"
+					>
 						<div class="relative">
 							<input
 								autocomplete="off"
@@ -60,7 +67,7 @@
 						<div class="relative text-center">
 							<button class="bg-blue-500 text-white rounded-md m-2 p-4 w-[100px]">SignIn</button>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
