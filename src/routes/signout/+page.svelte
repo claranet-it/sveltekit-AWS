1c7554ef7531e82323d9fcc73b0fe232f189f8e5
gioboa <giorgiob.boa@gmail.com>
2023-04-29 19:04:19 +0200

    change signout style

--- src/routes/signout/+page.svelte
+++ src/routes/signout/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import type { ActionData } from './$types';
+	import type { LayoutServerData } from '../$types';
+	import Button from '../../components/button/Button.svelte';
 
 	export let form: ActionData;
+	export let data: LayoutServerData;
 </script>
 
 <svelte:head>
@@ -9,17 +12,43 @@
 	<meta name="description" content="SignOut" />
 </svelte:head>
 
-<div class="m-8">
-	<form
-		class="pt-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
-		method="POST"
-		action="?/signOut"
-	>
-		<div class="relative text-center">
-			{#if form?.error}
-				<div class="text-red-500 text-sm">{form.error}</div>
-			{/if}
-			<button class="bg-blue-500 text-white rounded-md m-2 p-4 w-[100px]">Confirm SignOut</button>
+{#if data.session?.isAuthenticated}
+	<div>
+		<div class="min-h-[24rem] max-w-6xl m-auto rounded-lg p-4 space-y-4">
+			<div class="min-h-screen flex flex-col justify-center items-center">
+				<div
+					class="relative flex flex-col items-center rounded-[20px] w-[400px] sm:w-[500px] mx-auto p-8 bg-white bg-clip-border hover:shadow-2xl hover:shadow-orange-600"
+				>
+					<div class="relative flex h-32 w-full justify-center rounded-xl bg-cover">
+						<img
+							src="/account-background.png"
+							class="absolute flex h-32 w-full justify-center rounded-xl bg-cover"
+							alt="background"
+						/>
+						<div
+							class="absolute -bottom-12 flex h-[87px] w-[87px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400"
+						>
+							<img class="h-full w-full rounded-full" src="/user-icon.webp" alt="user icon" />
+						</div>
+					</div>
+					<div class="mt-16 flex flex-col items-center pb-4">
+						<h4 class="text-xl md:text-3xl font-bold">
+							{data.session?.fullName}
+						</h4>
+					</div>
+					<div class="flex flex-col items-center justify-center text-center">
+						<div class="text-sm md:text-lg">
+							<span class="px-2">{data.session?.email}</span>
+						</div>
+					</div>
+					<form method="POST" action="?/signOut">
+						<Button className="w-[200px]">Confirm Sign out</Button>
+					</form>
+					{#if form?.error}
+						<div class="text-red-500 text-sm">{form.error}</div>
+					{/if}
+				</div>
+			</div>
 		</div>
-	</form>
-</div>
+	</div>
+{/if}
