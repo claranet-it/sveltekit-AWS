4bf5c645295b3f25eafd3b73b9be8db0b5c59593
gioboa <giorgiob.boa@gmail.com>
2023-04-29 19:03:16 +0200

    add className to Button cmp

--- src/components/button/Button.svelte
+++ src/components/button/Button.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	export let className = ''
 </script>
 
 <div class="relative text-center">
-	<button class="bg-orange-600 text-white text-xl rounded-md m-2 p-4 w-[100px]">
+	<button class="bg-orange-600 text-white text-xl rounded-md m-2 p-4 w-[100px] {className}">
 		<slot />
 	</button>
 </div>
