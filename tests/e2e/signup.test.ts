3ab733029ce3a6e428ff29cb77c575383dffe6d2
Leonardo Montini <lion.48m@gmail.com>
2023-05-17 10:43:23 +0200

    Test e2e signup - confirm - login

--- tests/e2e/signup.test.ts
+++ tests/e2e/signup.test.ts
@@ -1,26 +1,40 @@
 import { expect, test } from '@playwright/test';
-import { MailSlurp } from "mailslurp-client";
+import { MailSlurp } from 'mailslurp-client';
 
 test.describe('Signup', () => {
-    test('Register new Cognito User', async ({ page }) => {
-        const apiKey = process.env.MAILSLURP_API_KEY;
-        expect(apiKey).toBeDefined();
+	test('Register new Cognito User', async ({ page }) => {
+		const apiKey = process.env.MAILSLURP_API_KEY;
+		expect(apiKey).toBeDefined();
 
-        const mailSlurp = new MailSlurp({ apiKey: apiKey as string });
-        const { id, emailAddress } = await mailSlurp.createInbox()
+		const mailSlurp = new MailSlurp({ apiKey: apiKey as string });
+		const { id, emailAddress } = await mailSlurp.createInbox();
 
-        const name = 'Test User';
-        const password = 'TestPassword123!';
+		const name = 'Test User';
+		const password = 'TestPassword123!';
 
-        await page.goto('/signup');
-        await page.locator('input[name="email"]').type(emailAddress, { delay: 100 });
-        await page.locator('input[name="name"]').type(name, { delay: 100 });
-        await page.locator('input[name="password"]').type(password, { delay: 100 });
-        await page.locator('button').click({ delay: 100 });
-        await page.screenshot({ path: 'screenshots/after-signup.png' });
+		await page.goto('/signup');
+		await page.locator('input[name="email"]').type(emailAddress);
+		await page.locator('input[name="name"]').type(name);
+		await page.locator('input[name="password"]').type(password);
+		await page.locator('button').click();
+		await page.screenshot({ path: 'screenshots/after-signup.png' });
 
-        // TODO: completare
+		const email = await mailSlurp.waitForLatestEmail(id);
 
-        await mailSlurp.deleteInbox(id);
-    });
+		const confirmationCode = email.body?.split(' ').pop() ?? '';
+
+		await page.locator('input[name="email"]').type(emailAddress);
+		await page.locator('input[name="code"]').type(confirmationCode);
+		await page.locator('button').click();
+
+		await page.screenshot({ path: 'screenshots/after-confirm.png' });
+
+		await page.locator('input[name="email"]').type(emailAddress);
+		await page.locator('input[name="password"]').type(password);
+		await page.locator('button').click();
+
+		await page.screenshot({ path: 'screenshots/after-signin.png' });
+
+		await mailSlurp.deleteInbox(id);
+	});
 });
